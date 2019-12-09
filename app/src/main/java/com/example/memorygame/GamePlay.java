5c3f2365ab82060d670e8d790d1c1df6b4447971
Abdelrahman Belal <abdelrahmanharazem@gmail.com>
2019-12-09 12:57:45 +0200

    add some features

--- app/src/main/java/com/example/memorygame/GamePlay.java
+++ app/src/main/java/com/example/memorygame/GamePlay.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -19,13 +21,24 @@ import java.util.Random;
 
 public class GamePlay extends AppCompatActivity {
 
+
     ArrayList<Integer>arrayList = new ArrayList<Integer>();
-    int[] imagesArray = {R.drawable.apple ,R.drawable.banana ,R.drawable.orange ,R.drawable.watermelon,R.drawable.apple ,R.drawable.banana ,R.drawable.orange ,R.drawable.watermelon};
+    int[] imagesArray = {R.drawable.apple ,R.drawable.banana ,R.drawable.orange ,R.drawable.pineapple,R.drawable.apple ,R.drawable.banana ,R.drawable.orange ,R.drawable.pineapple};
     ImageView imageView ;
     int counter =0 ,selection =1 ;
     int []cardSelections = new int[2];
     ImageView [] cardSelectionsViews = new ImageView[2];
     boolean allowed =true ;
+    int time = 0 ;
+    final Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            time++;
+            handler.postDelayed(this,1000);
+        }
+    };
+    MediaPlayer apple ,banana ,pineapple ,orange ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +48,11 @@ public class GamePlay extends AppCompatActivity {
             arrayList.add(new Integer(i));
         }
         Collections.shuffle(arrayList);
+        handler.post(runnable);
+        apple = MediaPlayer.create(this ,R.raw.apple);
+        orange = MediaPlayer.create(this ,R.raw.orangee);
+        banana = MediaPlayer.create(this ,R.raw.bananaa) ;
+        pineapple = MediaPlayer.create(this ,R.raw.pineapplee);
 
 
     }
@@ -58,6 +76,24 @@ public class GamePlay extends AppCompatActivity {
                 cardSelectionsViews[1] = imageView ;
 
                 if(cardSelections[0] == cardSelections[1]){
+                    try{
+                        if(cardSelections[0]==R.drawable.apple){
+                            apple.start();
+                        }
+                        else if(cardSelections[0]==R.drawable.banana){
+                            banana.start();
+                        }
+                        else if(cardSelections[0]==R.drawable.orange){
+                            orange.start();
+                        }
+                        else if(cardSelections[0]==R.drawable.pineapple){
+                            pineapple.start();
+                        }
+
+                    }catch (Exception e){
+                        Toast.makeText(getApplicationContext() ,"app was about to crash but rbna satar !!",Toast.LENGTH_SHORT).show();
+                    }
+
                     cardSelectionsViews[0].setEnabled(false);
                     cardSelectionsViews[1].setEnabled(false);
                     allowed = true ;
@@ -65,7 +101,7 @@ public class GamePlay extends AppCompatActivity {
                     if(counter == 4){
                         allowed = false ;
                         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                        builder.setTitle("Congratulations you win !!! ");
+                        builder.setTitle("Congratulations you win !!! your score is "+time+" seconds");
                         builder.setMessage("Do you want to play again ?");
                         builder.setCancelable(false);
                         builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
--- app/src/main/java/com/example/memorygame/MainActivity.java
+++ app/src/main/java/com/example/memorygame/MainActivity.java
@@ -6,12 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button startBtn ,dashBoardBtn ,exitBtn ;
     LinearLayout vlinearLayout2 ;
     Intent  gamePlayIntent ;
+    EditText playerName ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +25,18 @@ public class MainActivity extends AppCompatActivity {
         exitBtn = (Button) findViewById(R.id.exitBtn);
         vlinearLayout2 = (LinearLayout)findViewById(R.id.vLinearLayout2);
         gamePlayIntent = new Intent(this ,GamePlay.class);
+        playerName = (EditText)findViewById(R.id.playerName);
 
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(gamePlayIntent);
+                if(playerName.getText().length()>0){
+                    startActivity(gamePlayIntent);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Enter Player's  Name First",Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
